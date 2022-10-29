88c389b9b770e5b5fb2e7b2dad285d50ad5fa08b
Shivam Vadalia <shivamvadalia27@gmail.com>
2022-10-29 11:08:45 +0530

    Create 121_Best_Time_to_Buy_and_Sell_Stock.py

---
+++ Array/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(profit, maxP)
+            else:
+                l = r
+            r += 1
+        return maxP
+
+prices = [7,1,5,3,6,4]
+obj = Solution()
+print(obj.maxProfit(prices))
+
